5dbede72b520dbcf99f0dd770b61051a7c26135b
ssnyta <94050114+ssnyta@users.noreply.github.com>
2024-02-25 12:17:28 +0100

    CV-Flask-related-products

--- products/products.py
+++ products/products.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from collections import defaultdict
 from API.api import GetAllProducts, GetSingleProducts, SuggestProducts
 products_bp = Blueprint('products_bp', __name__,
     template_folder='templates',
@@ -13,13 +14,24 @@ def index():
 
 @products_bp.route('/products/<int:id>')
 
-def detailOfProduct(id):
-    data = GetSingleProducts(id)
-    return render_template('products/detail.html', detailOfPorduct = data)
-
 def getSuggestedProducts(id):
-    allproducts = GetAllProducts
-    produkty=[];
-    limit = 4
-    for produkt in allproducts:
-        produkt["category"] != allproducts["category"] & 
+    data = GetSingleProducts(id)
+    allproducts = GetAllProducts()
+    product = GetSingleProducts(id)
+    category = product["category"]
+    
+    suggested_products = []
+    category_products = defaultdict(list)
+    for p in allproducts:
+        category_products[p["category"]].append(p)
+        
+    for p in category_products[category]:
+        if p["id"] != id:
+            suggested_products.append(p)
+            if len(suggested_products) >= 4:
+                break
+    
+    return render_template("products/detail.html" ,DetailOfOneProduct = data, Suggested_Products = suggested_products )
+
+
+      
